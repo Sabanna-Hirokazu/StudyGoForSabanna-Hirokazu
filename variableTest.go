d3be4d0922cba08437cee4903a12457229272f4f
Sabanna-Hirokazu <hirokazu.h1126@gmail.com>
2019-12-24 00:31:46 +0900

    study about variable

---
+++ variableTest.go
@@ -0,0 +1,18 @@
+package main
+
+func main() {
+	var n int
+	println(n)
+	n = 10
+	println(n)
+	var k int = 20
+	println(k)
+	k++
+	k += 3
+	println(k)
+	var word string = "Hello, World"
+	println(word)
+	var a = "3" + "2"
+	b := 32.1
+	println(a, b)
+}
