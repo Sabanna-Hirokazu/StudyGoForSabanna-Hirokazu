31fdae5cd5af588667a84c51d79983ad73b55e13
Sabanna-Hirokazu <hirokazu.h1126@gmail.com>
2020-02-15 20:48:42 +0900

    add callback, closure sample.

--- functionTest.go
+++ functionTest.go
@@ -1,7 +1,26 @@
 package main
 
+var k int = 4
+
 func sample(tmp string) string {
 	return "'" + tmp + "'"
+}
+
+// CallBack  第二引数には引数が文字列、戻り地が文字列の関数が入る
+func sample2(tmp string, f func(string) string) string {
+	return f("sample2 - " + tmp)
+}
+
+// クロージャ
+func sample3() {
+	var i int = 4
+	f := func() {
+		i--
+		println(i)
+	}
+	for i > 1 {
+		f()
+	}
 }
 
 func main() {
@@ -18,4 +37,17 @@ func main() {
 	println(s3)
 	s4 := func(tmp string) string { return "+" + tmp + "+" }("OK")
 	println(s4)
+
+	// コールバック関数
+	s5 := sample2("World", sample)
+	println(s5)
+
+	// クロージャ
+	// pattern 1
+	sample3()
+	// pattern 2
+	for k > 1 {
+		k--
+		println(k)
+	}
 }
